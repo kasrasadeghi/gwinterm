956cdc43e0013fcbf723ea301401b64004d8ae08
kasrasadeghi <kasra.sadeghi1@gmail.com>
2017-07-24 22:43:31 -0500

    I have no idea what's wrong.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <gtkmm/application.h>
 #include <gtkmm/window.h>
 #include "Clock.h"
+#include <glibmm/optioncontext.h>
+#include <glibmm/optiongroup.h>
+#include <glibmm/optionentry.h>
+
+using std::string;
 
 int main(int argc, char *argv[])
 {
@@ -11,13 +16,19 @@ int main(int argc, char *argv[])
 //
 //  return app->run(window);
 
-  auto app = Gtk::Application::create(argc, argv, "kaz.clock.example");
-  Gtk::Window win;
-  win.set_title("Cairomm Clock");
-
-  Clock c;
-  win.add(c);
-  c.show();
-
-  return app->run(win);
+//  auto app = Gtk::Application::create(argc, argv, "kaz.clock.example");
+//  Gtk::Window win;
+//  win.set_title("Cairomm Clock");
+//
+//  Clock c;
+//  win.add(c);
+//  c.show();
+//
+//  return app->run(win);
+  Glib::OptionContext context(nullptr);
+  Glib::OptionEntry entry = {};
+  Glib::OptionGroup optionGroup;
+  optionGroup.add_entry(entry);
+  context.add_group(optionGroup);
+  string term = "xterm-termite";
 }
